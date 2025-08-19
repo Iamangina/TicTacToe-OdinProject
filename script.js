8ac15576caeda1021cca5cabedf9777966b321e2
Iamangina <203669321+Iamangina@users.noreply.github.com>
2025-08-19 18:53:34 +0000

    add-web-ui-and-game-functions

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const newPlayer = function(playerName, symbol){
+const Game = (function(){
+
+const newPlayer = function(playerName, symbol, color){
         return{
             playerName : playerName,
-            symbol : symbol
+            symbol : symbol,
+            color : color
         }
-}
+};
 
-const X = newPlayer("X", "X");
-const O = newPlayer("O", "O");
+const X = newPlayer("X", "X", "rgb(158, 210, 235)");
+const O = newPlayer("O", "O", "rgb(243, 158, 132)");
 
 
 const Gameboard = {
@@ -24,6 +27,7 @@ const Gameboard = {
         }
         return false;
     },
+
     winningCombinations : [
         [0, 1, 2],
         [3, 4, 5],
@@ -43,27 +47,73 @@ checkWin(symbol) {
 
 let currentPlayer = X;
 
-function makeMove(index){
-    if(Gameboard.setCell(index, currentPlayer.symbol)){
-        currentPlayer = currentPlayer === X ? O : X;
-    }
-}
+const resetBtn = document.querySelector(".reset");
+const boardDiv = document.querySelector("#board");
 
+let srcX = 0;
+let srcO = 0;
+
+function renderGameboard(){
+    const scoreX = document.querySelector(".scoreX");
+    const scoreO = document.querySelector(".scoreO");
+
+        Gameboard.board.forEach((cell, index)=>{
+            const cellDiv = document.createElement('div');
+            cellDiv.classList.add("cell");
+            cellDiv.textContent = cell;
+
+            
+            cellDiv.addEventListener("click", function(){
+                function makeMove(index){
+                if(Gameboard.setCell(index, currentPlayer.symbol)){
+                    cellDiv.textContent = currentPlayer.symbol;
+                    cellDiv.style.color = currentPlayer.color;
+                    currentPlayer = currentPlayer === X ? O : X;
+                }
+                } makeMove(index);
+                
+                function disableBoard(){
+                    const cells = document.querySelectorAll(".cell");
+                    cells.forEach(cell => {
+                        cell.replaceWith(cell.cloneNode(true));
+                    });
+                }
+
+                function checkWinner(){
+                    if(Gameboard.checkWin(X.symbol)){
+                        console.log("X win!");
+                        srcX++;
+                        scoreX.textContent = `${srcX}`;
+                        disableBoard();
+                    }
+                    else if(Gameboard.checkWin(O.symbol)){
+                        console.log("O win!");
+                        srcO++;
+                        scoreO.textContent = `${srcO}`;
+                       disableBoard();
+                    };
+                }; checkWinner();  
+
+                if(srcX===5 || srcO ===5){
+                    document.querySelector(".reset").disabled = true;
+                }
+        });
+        boardDiv.appendChild(cellDiv);       
+    }); 
+};
 Gameboard.initBoard();
+renderGameboard();
 
+resetBtn.addEventListener("click", function(){
+    boardDiv.innerHTML = "";
+    Gameboard.board = [];
+    Gameboard.initBoard();
+    renderGameboard();
+    console.log(Gameboard.board)
+})
 
-makeMove(0);
-makeMove(3);
-makeMove(6);
-makeMove(4);
-makeMove(2);
-makeMove(5);
-
- if(Gameboard.checkWin(X.symbol)){
-    console.log("X win!");
-    }
-    else if(Gameboard.checkWin(O.symbol)){
-        console.log("O win!")
-    }
 
 console.log(Gameboard.board)
+
+
+})();
